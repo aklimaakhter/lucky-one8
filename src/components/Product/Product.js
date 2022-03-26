2962527a3048115c0ed56b62ff324558f81ca450
aklimaakhter <aklimawebd@gmail.com>
2022-03-26 13:28:25 +0600

    card added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
     const { id, name, price, img } = props.product;
     return (
-        <div>
-            <img src={img} alt=""></img>
-            <p>{name}</p>
-            <p>Price: {price}</p>
+        <div className='product'>
+            <div>
+                <img src={img} alt=""></img>
+                <div className='product-info'>
+                    <h2>{name}</h2>
+                    <p><small>Id :{id}</small></p>
+                    <p>Price :{price}</p>
+                </div>
+            </div>
+            <button>
+                <p className='btn-text'>Add To Cart</p>
+            </button>
         </div>
     );
 };
